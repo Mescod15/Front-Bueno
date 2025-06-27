62766e2921e992b433e2726b8f674d948cd0b9ca
Marcos <mjmarco74@gmail.com>
2025-06-27 10:03:23 -0600

    login v2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     setError(null);
 
     try {    
-      const res = await fetch(`${API_URL}/user/auth/login/`, {
+      const res = await fetch(`${API_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
